8dff637a33ee2e6b5af74c964fd24ab7b7360f6f
Adeel Iqbal <iadeel26@gmail.com>
2025-09-22 05:08:47 -0700

    Loops program and compile file added

---
+++ Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count <=5){
+            System.out.println(count);
+            count++;
+        }
+
+        // for loop
+        for(int a = 1; a <= 5; a++){
+            System.out.println(a);
+        }
+    }
+}
+
