8e14b443cf5059ac224a27fe15742081f1771495
Adeel Iqbal <iadeel26@gmail.com>
2025-09-28 23:31:16 -0700

    Sum of first 10 natural numbers program

---
+++ SumN10.java
@@ -0,0 +1,9 @@
+public class SumN10 {
+    public static void main(String[] args) {
+        int Sum = 0;
+        for(int num=1; num <= 10; num++){
+            Sum = Sum + num;
+        }
+        System.out.println("Sum of first 10 natural numbers is " + Sum);
+    }
+}
